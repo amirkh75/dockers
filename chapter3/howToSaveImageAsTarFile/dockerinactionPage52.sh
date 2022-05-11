08a08176e87e4fa6b868db3b14091ff490102f7c
amirkh75 <amirp.1375@gmail.com>
2022-05-11 18:31:53 +0200

    made it tidy.

--- chapter3/howToSaveImageAsTarFile/dockerinactionPage52.sh
+++ chapter3/howToSaveImageAsTarFile/dockerinactionPage52.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-alias doseExist='source /home/amirkh/Documents/dockers/howToSaveImageAsTarFile/ifExist.sh'
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+alias doseExist='source $SCRIPT_DIR/ifExist.sh'
+echo 'source $SCRIPT_DIR/ifExist.sh'
 
 doseExist
 docker pull busybox:latest
