1921d994e3a820ad00bb518999e911708efe6a1f
amirkh75 <amirp.1375@gmail.com>
2022-05-14 17:36:35 +0200

    add chapter5 and some moves ;).

---
+++ dockerinaction/chapter5/userDefineBridge/page85.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+clear
+
+docker network create \
+    --driver bridge \
+    --label project=dockerinaction \
+    --label chapter=5 \
+    --attachable \
+    --scope local \
+    --subnet 10.0.42.0/24 \
+    --ip-range 10.0.42.128/25 \
+    user-network
+
+
+docker network ls | grep user-network
+
+docker network rm user-network
